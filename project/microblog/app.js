40744884ad173e815a5b0d7b0e6b039679bca7cb
luquanhong <luquanhong@gmail.com>
2014-01-04 11:14:53 +0800

    test

--- project/microblog/app.js
+++ project/microblog/app.js
@@ -96,6 +96,7 @@ app.get('/helper', function(req, res){
 
 */
 
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
